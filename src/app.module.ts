803195123281cad1a9ddb23e7529f7c439130919
babyyoda62406 <babyyoda62406@gmail.com>
2024-08-12 00:19:25 -0400

    config: TypeORM +  PG initialiced

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envs } from './config';
 
 @Module({
-  imports: [OrderModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: envs.DB_HOST,
+      port: envs.DB_PORT,
+      username: envs.DB_USER,
+      password: envs.DB_PASS,
+      database: envs.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }), OrderModule],
 })
 export class AppModule {}
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,10 +5,20 @@ import *  as joi from 'joi'
 
 interface EnvsVars {
     PORT: number;
+    DB_HOST: string;
+    DB_PORT: number;
+    DB_USER: string;
+    DB_PASS: string;
+    DB_NAME: string;
 } 
 
 const envSchema = joi.object({
     PORT: joi.number().required(),
+    DB_HOST: joi.string().required(),
+    DB_PORT: joi.number().required(),
+    DB_USER: joi.string().required(),
+    DB_PASS: joi.string().required(),
+    DB_NAME: joi.string().required(),
 }).unknown(true);
 ;
 
@@ -19,5 +29,10 @@ if(error) {
 }
 
 export const envs: EnvsVars = { 
-    PORT: value.PORT
+    PORT: value.PORT, 
+    DB_HOST: value.DB_HOST, 
+    DB_PORT: value.DB_PORT, 
+    DB_USER: value.DB_USER, 
+    DB_PASS: value.DB_PASS, 
+    DB_NAME: value.DB_NAME,
 };
